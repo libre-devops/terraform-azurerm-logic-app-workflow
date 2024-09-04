88746d7b97834473ae1fbc9af1070389fcac3f59
Craig Thacker <craigthackerx@gmail.com>
2024-09-04 17:37:41 +0100

    update

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
-#
+resource "azurerm_logic_app_workflow" "workflows" {
+  for_each                     = { for app in var.logic_app_workflows : app.name => app }
+
+  location            = each.value.location
+  name                = each.value.name
+  resource_group_name = each.value.rg_name
+  tags = each.value.tags
+
+
+  dynamic "access_control" {
+    for_each = ""
+    content {}
+  }
+}
--- variables.tf
+++ variables.tf
@@ -17,3 +17,44 @@ variable "tags" {
   type        = map(string)
   description = "A map of the tags to use on the resources that are deployed with this module."
 }
+
+variable "logic_app_workflows" {
+  description = "The list of object settings for logic app workflows"
+  type = list(object({
+    name     = string
+    rg_name  = string
+    location = string
+    tags = map(string)
+    enabled = optional(bool, true)
+    integration_service_environment_id = optional(string)
+    logic_app_integration_account_id = optional(string)
+    identity_type = optional(string)
+    identity_ids = optional(list(string))
+    workflow_schema = optional(string)
+    workflow_parameters = optional(map(string))
+    workflow_version = optional(string, "1.0.0.0")
+    parameters = optional(map(string))
+    access_control = optional(object({
+      action = optional(object({
+        allowed_caller_ip_address_ranges = list(string)
+      }))
+      content = optional(object({
+        allowed_caller_ip_address_ranges = list(string)
+      }))
+      trigger = optional(object({
+        allowed_caller_ip_address_ranges = list(string)
+        open_authentication_policy = optional(object({
+          name = string
+          claim = optional(object({
+            name = string
+            value = string
+          }))
+        }))
+      }))
+      workflow_management = optional(object({
+        allowed_caller_ip_address_ranges = list(string)
+      }))
+    }))
+  }))
+}
+
